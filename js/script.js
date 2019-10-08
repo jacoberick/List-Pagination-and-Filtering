bd0bb6d5e17d9f88caa19dc83e8de4f76921426c
Jacob White <jacobewhitedev@gmail.com>
2019-10-08 01:24:14 -0500

    added global variables, showPage(), and appendPageLinks()

--- js/script.js
+++ js/script.js
@@ -2,49 +2,49 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+const studentList = document.getElementsByClassName("student-item");
+const perPage = 10;
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+const showPage = (list, page) => {
+  const start = page * perPage - perPage;
+  const end = page * perPage;
 
+  for (i = 0; i < list.length; i++) {
+    if (i >= start && i < end) {
+      list[i].hidden = false;
+    } else {
+      list[i].hidden = true;
+    }
+  }
+};
 
+showPage(studentList, 1);
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
+/***
+   Create the `appendPageLinks function` to generate, append, and add
    functionality to the pagination buttons.
 ***/
 
+const appendPageLinks = list => {
+  const pages = Math.ceil(list.length / perPage);
+  const pageSelect = document.querySelector(".page");
 
+  let paginationList = `<div class='pagination'><ol>`;
+  for (i = 1; i <= pages; i++) {
+    paginationList += `<li class='button-pg'>`;
+    paginationList +=
+      i === 1
+        ? `<a onclick='pageHopper' class="active" (${i})">${i}</a>`
+        : `<a onclick='pageHopper' (${i})">${i}</a>`;
+    paginationList += `</li>`;
+  }
+  paginationList += `</ol></div>`;
+  pageSelect.innerHTML += paginationList;
+};
+appendPageLinks(studentList);
 
-
-
+const pageHopper = newPage => {
+  const buttons = document.getElementsByClassName("button-pg");
+};
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
