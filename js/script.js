88647362313a4bb57c4097b21b2210701060b9c6
Jacob White <jacobewhitedev@gmail.com>
2019-10-16 19:37:33 -0500

    added appendSearchBar()

--- js/script.js
+++ js/script.js
@@ -28,10 +28,13 @@ showPage(studentList, 1);
 const appendPageLinks = list => {
   const pages = Math.ceil(list.length / perPage);
   const pageSelect = document.querySelector(".page");
-
+  //construction of page links begins here
   let paginationList = `<div class='pagination'><ol>`;
   for (i = 1; i <= pages; i++) {
     paginationList += `<li class='button-pg'>`;
+    //if page one is selected, then the active class is added and the first 10 students are shown
+    //else if another page link is clicked, then that set of students will be displayed
+    //on click of page links `pageHopper` is called using selected page number
     paginationList +=
       i === 1
         ? `<a onclick='pageHopper(${i})' class="active">${i}</a>`
@@ -43,14 +46,31 @@ const appendPageLinks = list => {
 };
 appendPageLinks(studentList);
 
+//`pageHopper` adds and removes classes to page links
 const pageHopper = newPage => {
   const buttons = document.getElementsByClassName("button-pg");
   for (i = 0; i < buttons.length; i++) {
     let aTag = buttons[i].children[0];
+    //if newPage is equal to the selected page link in appendPageLinks
+    //then the class of 'active' is applied
+    //else if the page number does not equal newPage, the 'active' class is removed
     i + 1 === newPage
       ? aTag.classList.add("active")
       : aTag.classList.remove("active");
   }
   showPage(studentList, newPage);
 };
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+
+//adds search form to the DOM
+const appendSearchBar = () => {
+  const headerSelect = document.querySelector(".page-header");
+  let insrtSearch = `<div class='student-search'>`;
+  insrtSearch += `<input type='text' onkeyup='searchStudents()' id='search' onkeyup='sortThru()' placeholder='Search for students...'>`;
+  insrtSearch += `<button>Search</button>`;
+  insrtSearch += `</div>`;
+  headerSelect.innerHTML += insrtSearch;
+};
+appendSearchBar();
+
+//filters through Students
+const searchStudents = () => {};
